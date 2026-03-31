4daac9c2517c7a4bcc5386ef2befe6064bf69975
WorksaseItsgood <worksaseitsgood@github.com>
2026-03-31 23:00:17 +0000

    fix: case.js syntax

--- commands/moderation/case.js
+++ commands/moderation/case.js
@@ -9,15 +9,15 @@ module.exports = {
     .setName('case')
     .setDescription('View or manage a case')
     .addIntegerOption(option => option.setName('caseid').setDescription('Case ID').setRequired(false)),
-  
+
   async execute(interaction, client) {
     const caseId = interaction.options.getInteger('caseid');
-    
+
     const embed = new EmbedBuilder()
       .setTitle('📁 Case View')
-      .setDescription(caseId ? `Case #${caseId}\n• User: Example#1234\n• Action: Warn\n• Reason: Spam\n• Moderator: Admin\n• Date: Today`)
+      .setDescription(caseId ? 'Case #' + caseId + '\n• User: Example#1234\n• Action: Warn\n• Reason: Spam\n• Moderator: Admin\n• Date: Today' : 'Please provide a case ID!')
       .setColor(0x0000ff);
-    
+
     await interaction.reply({ embeds: [embed] });
   }
 };
